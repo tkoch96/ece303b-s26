ba98056416c7e299a2e378feaf26b9400d8d8111
Thomas Koch <tomkoch123@gmail.com>
2026-03-17 17:25:07 -0400

    Fixed an error where we didn't have enough dns server arguments in the skeleton

--- dns_server.py
+++ dns_server.py
@@ -3,12 +3,13 @@
 import sys
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: python dns_server.py [dns_server_ip_address] [web_server_address]")
+    if len(sys.argv) != 4:
+        print("Usage: python dns_server.py [dns_server_ip_address] [web_server_ip_address] [web_server_address]")
         sys.exit(1)
 
     dns_ip = sys.argv[1]
     web_ip = sys.argv[2]
+    web_host = sys.argv[3]
     port = 5353
 
     ## TODO -- implement your dns server
