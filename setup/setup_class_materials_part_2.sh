dff1894acfd64ae687e70365262fe28f3c1977ca
Tom <tomkoch123@gmail.com>
2026-04-14 13:58:11 +0000

    Important controller linkage line in setup script

--- setup/setup_class_materials_part_2.sh
+++ setup/setup_class_materials_part_2.sh
@@ -1,3 +1,4 @@
 echo "[*] Installing Mininet and Open vSwitch..."
 sudo apt-get install -y mininet openvswitch-switch
 sudo systemctl start openvswitch-switch
+sudo ln -s /usr/bin/ovs-testcontroller /usr/bin/ovs-controller
